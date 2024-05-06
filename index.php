787bd4a3c869f4196e0fbafc619aa782110d8080
Evgen4X <131477076+Evgen4X@users.noreply.github.com>
2024-05-06 12:49:19 +0200

    v -0.86

--- index.php
+++ index.php
@@ -20,9 +20,10 @@ session_start();
 
     echo "<main><div id='main'>";
 
+    echo "select ksiazki.tytul, concat(substr(autor.imie, 1, 1), '. ', autor.nazwisko), ksiazki.cena,  ksiazki.gatunek, ksiazki.jezyk_ksiazki, ksiazki.rok_wydania, ksiazki.id_ksiazki from ksiazki join autor using (id_autora) order by ksiazki.id_ksiazki ".(isset($_POST['sort-cena']) && $_POST['sort-cena'] != '2' ? ", ksiazki.cena ".(isset($_POST['sort-cena']) && $_POST['sort-cena'] == '1' ? 'asc' : 'desc') : "").(isset($_POST['sort-autor']) && $_POST['sort-autor'] != '2' ? ", autor.imie ".(isset($_POST['sort-autor']) && $_POST['sort-autor'] == '1' ? 'asc' : 'desc') : "").(isset($_POST['sort-rok-wydania']) && $_POST['sort-rok-wydania'] != '2' ? ", ksiazki.rok_wydania ".(isset($_POST['sort-rok-wydania']) && $_POST['sort-rok-wydania'] == '1' ? 'asc' : 'desc') : "")." limit $i, 1;";
 
     for($i = 1; $i < 100; ++$i){
-        $book = mysqli_query($id, "select ksiazki.tytul, concat(substr(autor.imie, 1, 1), '. ', autor.nazwisko), ksiazki.cena,  ksiazki.gatunek, ksiazki.jezyk_ksiazki, ksiazki.rok_wydania, ksiazki.id_ksiazki from ksiazki join autor using (id_autora) limit $i, 1;");
+        $book = mysqli_query($id, "select ksiazki.tytul, concat(substr(autor.imie, 1, 1), '. ', autor.nazwisko), ksiazki.cena,  ksiazki.gatunek, ksiazki.jezyk_ksiazki, ksiazki.rok_wydania, ksiazki.id_ksiazki from ksiazki join autor using (id_autora) ".(isset($_POST['sort-cena']) && $_POST['sort-cena'] != '2' ? "order by ksiazki.cena ".(isset($_POST['sort-cena']) && $_POST['sort-cena'] == '1' ? 'asc' : 'desc') : "").(isset($_POST['sort-autor']) && $_POST['sort-autor'] != '2' ? (isset($_POST['sort-cena']) && $_POST['sort-cena'] != '2' ? ", " : 'order by ')."autor.imie ".(isset($_POST['sort-autor']) && $_POST['sort-autor'] == '1' ? 'asc' : 'desc') : "").(isset($_POST['sort-rok-wydania']) && $_POST['sort-rok-wydania'] != '2' ? (isset($_POST['sort-cena']) && $_POST['sort-cena'] != '2' || isset($_POST['sort-cena']) && $_POST['sort-cena'] != '2' ? ", " : 'order by ')."ksiazki.rok_wydania ".(isset($_POST['sort-rok-wydania']) && $_POST['sort-rok-wydania'] == '1' ? 'asc' : 'desc') : "")." limit $i, 1;");
         if($book){
             $data = mysqli_fetch_row($book);
             echo "<div class='ksiazka'>
@@ -55,15 +56,34 @@ session_start();
             <fieldset>
                 <legend>Sortuj</legend>
                 <table>
-                    <tr><td>Cena</td><td><input type='checkbox' name='sortuj-cena'></td></tr>
+                    <tr><td>Cena</td><td><div class='checkbox".(!isset($_POST['sort-cena']) || $_POST['sort-cena'] == '0' ? '0' : $_POST['sort-cena'])."' onclick='toggle(this, \"sort-cena\");'></div><input type='hidden' value='".(isset($_POST['sort-cena']) ? $_POST['sort-cena'] : '2')."' name='sort-cena' id='sort-cena'></td></tr>
+                    <tr><td>Autor</td><td><div class='checkbox".(!isset($_POST['sort-autor']) || $_POST['sort-autor'] == '0' ? '0' : $_POST['sort-autor'])."' onclick='toggle(this, \"sort-autor\");'></div><input type='hidden' value='".(isset($_POST['sort-autor']) ? $_POST['sort-autor'] : '2')."' name='sort-autor' id='sort-autor'></td></tr>
+                    <tr><td>Rok wydania</td><td><div class='checkbox".(!isset($_POST['sort-rok-wydania']) || $_POST['sort-rok-wydania'] == '0' ? '0' : $_POST['sort-rok-wydania'])."' onclick='toggle(this, \"sort-rok-wydania\");'></div><input type='hidden' value='".(isset($_POST['sort-rok-wydania']) ? $_POST['sort-rok-wydania'] : '2')."' name='sort-rok-wydania' id='sort-rok-wydania'></td></tr>
                 </table>
             </fieldset>
-            <input type='submit'>
+            <input type='submit' value='Zapisz'>
         </form>
     </aside>";
       
     echo "<footer>Created by <a href='https://github.com/Evgen4X'>Evgen4X</a></footer>";
 
     ?>
+
+    <script>
+        function toggle(div, name){
+            if(div.classList.contains('checkbox0')){
+                div.classList.remove('checkbox0');
+                div.classList.add('checkbox1');
+            } else if(div.classList.contains('checkbox1')){
+                div.classList.remove('checkbox1');
+                div.classList.add('checkbox2');
+            } else if(div.classList.contains('checkbox2')){
+                div.classList.remove('checkbox2');
+                div.classList.add('checkbox0');
+            }
+            document.getElementById(name).value = div.classList.contains('checkbox1') ? '1' : div.classList.contains('checkbox2') ? '2' : '0';
+        }
+    </script>
+
 </body>
 </html>
